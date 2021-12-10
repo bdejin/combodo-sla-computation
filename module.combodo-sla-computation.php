37402d27e7691913c8700a5e1ed88caea033a7da
acognet <anne-catherine.cognet@combodo.com>
2021-12-10 09:08:11 +0100

    Remove -dev in version number

--- module.combodo-sla-computation.php
+++ module.combodo-sla-computation.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-sla-computation/2.4.0-dev',
+	'combodo-sla-computation/2.4.0',
 	array(
 		// Identification
 		//
